4ecd53fd2beeffaab0e188f1b2d88119d5063372
Ajit Unnam <ajit.unnam@gmail.com>
2016-03-19 14:12:13 -0700

    05b q5 fixed

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,22 @@
-PLACE YOUR CODE HERE
+#!/usr/bin/python3
+import csv
+import sys
+
+facultyFile = open('faculty.csv')
+facultyReader = csv.reader(facultyFile)
+facultyData = list(facultyReader)
+
+def emailList(data):
+    emails = []
+    for row in data[1:]:
+        emails += [row[3]]
+    return emails
+
+def csv_writer(data, path):
+    with open(path, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        for line in data:
+            writer.writerow([line])
+
+csv_writer(emailList(facultyData), 'emails.csv')
+
