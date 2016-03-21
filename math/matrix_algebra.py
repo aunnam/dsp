35cc16f25613a807e0309804f298426725a0204f
Ajit Unnam <ajit.unnam@gmail.com>
2016-03-21 11:52:08 -0700

    Added completed matrix algebra worksheet

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -24,14 +24,14 @@ print("2.4) u * v = ", np.inner(u,v))
 print("2.5) ||u|| = ", np.linalg.norm(u))
 
 # print("3.1) A + C = ", A+C) #throws an error
-print("3.1) A + C = Not Defined")
+print("3.1) A + C = not defined")
 print("3.2) A - C^T = ", A - C.transpose())
 print("3.3) C^T + 3D = ", C.transpose() + 3*D)
 print("3.4) B A = ", np.dot(B,A))
 # print("3.5) B A^T = ", np.dot(B,A.transpose())) #throws an error
-print("3.5) B A^T = Not Defined")
+print("3.5) B A^T = not defined")
 # print("3.6) B C = ", np.dot(B,C)) #throws an error
-print("3.6) B C = Not Defined")
+print("3.6) B C = not defined")
 print("3.7) C B = ", np.dot(C,B))
 print("3.8) B^4 = ", np.dot(B,np.dot(B,np.dot(B,B))))
 print("3.9) A A^T = ", np.dot(A, A.transpose()))
